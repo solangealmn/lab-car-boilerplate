2157ee1097faf9334cbfa68e4615047bb34cc3ee
solangealmn <solangealmn@gmail.com>
2018-07-30 18:53:34 -0300

    Responsivo incompleto.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 // La funcionalidad de tu proyecto
+// Initialize and add the map
+function initMap() {
+  var ibmec = {lat: -23.557605, lng: -46.662384};
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 16, center: ibmec});
+  var marker = new google.maps.Marker({position: ibmec, map: map});
+}
